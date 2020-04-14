1bb878f0aa3a67bc6e34f86a87a5e49c9250d6e9
emirate691 <aderojus@gmail.com>
2020-04-14 11:49:07 +0100

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,27 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    tasks: [
+      {
+        id: 1,
+        list: "Go out for shopping",
+      },
+      {
+        id: 2,
+        list: "Take a Stroll",
+      },
+      {
+        id: 3,
+        list: "Buy me sharwama",
+      },
+    ],
+    newTask: null,
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  mutations: {},
+  actions: {},
+  modules: {},
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,114 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="containter" id="text-left md-6 mx-auto">
+    <div class="home">
+      <h1>Todo List App</h1>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="text-left md-6 mx-auto">
+            <div class="form-group">
+              <input
+                type="text"
+                placeholder="Enter your name"
+                class="form-control"
+                v-model="newTask"
+              />
+              <button
+                type="button"
+                class="btn btn-success"
+                v-on:click="addTask"
+              >
+                Submit
+              </button>
+            </div>
+
+            <ul class="list-group">
+              <li
+                button
+                class="list-group-item"
+                v-for="task in tasks"
+                :key="task.id"
+              >
+                {{ task.list }}
+                <span class="close">
+                  <button
+                    @click="editTask(task)"
+                    class="btn btn-primary"
+                    id="btn-1"
+                  >
+                    Edit</button
+                  ><button
+                    @click="deleteTask(task)"
+                    class="btn btn-danger"
+                    id="btn-2"
+                  >
+                    Delete
+                  </button>
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data() {
+    return {
+      newTask: null,
+    };
+  },
+  computed: {
+    tasks() {
+      return this.$store.state.tasks;
+    },
+  },
+  methods: {
+    addTask() {
+      this.tasks.push({ newTask: this.newTask });
+      this.newTask = null;
+    },
+    deleteTask(task) {
+      this.tasks.splice(this.tasks.indexOf(task), 1);
+    },
+    editTask(list, id) {
+      this.id = id;
+      this.task = (list, true);
+      alert("alert edit");
+      console.log(list);
+    },
+  },
+};
 </script>
+<style>
+.container-fluid {
+  border: 1px solid;
+  height: 400px;
+  width: 50%;
+}
+.btn {
+  width: 100%;
+  margin-top: 5px;
+}
+.list-group {
+  width: 600px;
+  height: 30px;
+  margin: 0;
+}
+#btn-1 {
+  width: 40%;
+}
+#btn-2 {
+  width: 50%;
+  margin-left: 5px;
+}
+
+.form-group {
+  margin-top: 40px;
+}
+</style>
